7c2830c666fd2a0ac6b7f2ec3ebca2a5f1d49454
vxrtualy <m.singh.khinda07@gmail.com>
2021-12-27 19:08:14 +0000

    deleted calc.py and modify main-calc

--- main-calc.py
+++ main-calc.py
@@ -1,10 +1,9 @@
-operation=input("Choose your operation: \n+     for addition\n-     for subtraction\n*     for multiplication\n/     for division\n")
+operation=input("Choose your operation: \n+     for addition\n-     for subtraction\n*     for multiplication\n/     for division\nOperation: ")
 question=input("Enter your question: ")
 if operation == "+":
     a=question.find("+")
     if not "+" in question:
         print("Invalid question- that doesn't have a addition sign (+) in it.")
-    question=input("Enter your addition question here: ")
     a=question.find("+")
     b=question[:a]
     c=question[a+1:]
@@ -15,7 +14,6 @@ if operation == "-":
     a=question.find("-")
     if not "-" in question:
         print("Invalid question- that doesn't have a subtraction sign (-) in it.")
-    question=input("Enter your subtraction question here: ") 
     a=question.find("-")
     b=question[:a]
     c=question[a+1:]
@@ -26,7 +24,6 @@ if operation == "*":
     a=question.find("*")
     if not "*" in question:
         print("Invalid question- that doesn't have a multiplication sign (*) in it.")
-    question=input("Enter your multiplication question here: ")
     a=question.find("*")
     b=question[:a]
     c=question[a+1:]
@@ -37,10 +34,9 @@ if operation == "/":
     a=question.find("/")
     if not "/" in question:
         print("Invalid question- that doesn't have a division sign (/) in it.")
-    question=input("Enter your division question here: ")
     a=question.find("/")
     b=question[:a]
     c=question[a+1:]
     if "/" in question:
         d=int(b) / int(c)
-        print("       ", d, "\n", b, "|¯¯¯", c, "¯¯¯")
+        print("       ", d, "\n", c, "|¯¯¯", b, "¯¯¯")
