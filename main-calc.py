dc15d135eacc1c0ee85c4e39e3f38560c73ab8b9
vxrtualy <m.singh.khinda07@gmail.com>
2021-12-27 18:58:54 +0000

    made a menu and working calc for all 4 operations

--- main-calc.py
+++ main-calc.py
@@ -1,12 +1,46 @@
-question=input("Enter your question here: ")
-a=question.find("+", "-", "*" or "/")
-print(a)
-if not "+" in question:
-    print("Invalid question- that doesn't have a addition sign (+) in it.")
-question=input("Enter your addition question here: ")
-a=question.find("+")
-b=question[:a]
-c=question[a+1:]
-if "+" in question:
-    d=int(b) + int(c)
-    print("\n", b, "+", "\n", c, "\n", "____", "\n", d)
+operation=input("Choose your operation: \n+     for addition\n-     for subtraction\n*     for multiplication\n/     for division\n")
+question=input("Enter your question: ")
+if operation == "+":
+    a=question.find("+")
+    if not "+" in question:
+        print("Invalid question- that doesn't have a addition sign (+) in it.")
+    question=input("Enter your addition question here: ")
+    a=question.find("+")
+    b=question[:a]
+    c=question[a+1:]
+    if "+" in question:
+        d=int(b) + int(c)
+        print("\n", b, "+", "\n", c, "\n", "____", "\n", d)
+if operation == "-":
+    a=question.find("-")
+    if not "-" in question:
+        print("Invalid question- that doesn't have a subtraction sign (-) in it.")
+    question=input("Enter your subtraction question here: ") 
+    a=question.find("-")
+    b=question[:a]
+    c=question[a+1:]
+    if "-" in question:
+        d=int(b) - int(c)
+        print("\n", b, "-", "\n", c, "\n", "____", "\n", d)
+if operation == "*":
+    a=question.find("*")
+    if not "*" in question:
+        print("Invalid question- that doesn't have a multiplication sign (*) in it.")
+    question=input("Enter your multiplication question here: ")
+    a=question.find("*")
+    b=question[:a]
+    c=question[a+1:]
+    if "*" in question:
+        d=int(b) * int(c)
+        print("\n", b, "*" "\n", c, "\n", "____", "\n", d)
+if operation == "/":
+    a=question.find("/")
+    if not "/" in question:
+        print("Invalid question- that doesn't have a division sign (/) in it.")
+    question=input("Enter your division question here: ")
+    a=question.find("/")
+    b=question[:a]
+    c=question[a+1:]
+    if "/" in question:
+        d=int(b) / int(c)
+        print("       ", d, "\n", b, "|¯¯¯", c, "¯¯¯")
